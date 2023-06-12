45a263435f56dc9cb283938e246f482a8275c305
Rogerslug <rogelio.020496@gmail.com>
2023-06-11 23:29:45 -0600

    Añadido nueva interfaz a homepage con prueba de formulario

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './HomePage.css';
 
 function HomePage() {
+    const [searchTerm, setSearchTerm] = useState('');
+    
+    const handleSearch = (event) => {
+        event.preventDefault();
+        console.log(searchTerm);
+        //Poner petición Api para buscar productos
+    };
+
     return (
-        <main>
-        <h2>¡Bienvenido a nuestra tienda online!</h2>
-        <p>Aquí puedes encontrar una gran variedad de productos a los mejores precios</p>
-        </main>
+        <div className='homePage'>
+            <h2>¡Bienvenido a nuestra tienda online!</h2>
+            <p>Aquí puedes encontrar una gran variedad de productos a los mejores precios</p>
+            <form onSubmit={handleSearch}>
+                <input
+                    type="text"
+                    placeholder="Busqueda para un producto"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button type='submit'>Buscar</button>
+            </form>
+        </div>
     );
 }
 
